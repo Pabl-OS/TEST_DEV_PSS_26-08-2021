5071c5b22219d6b9470bb99c011c637e73a572dd
Pabl-OS <pablito99sant@gmail.com>
2021-07-06 23:47:50 -0500

    first intent for resolve connection

--- models/generales.js
+++ models/generales.js
@@ -1,7 +1,8 @@
 const db = require('../db')
 
 //  model for get data general
-const getGenerals = () =>{
+
+const getGenerals = async () =>{
     return new Promise((resolve, reject)=>{
         db.query(`SELECT det_gen.c_min AS 'monto_minimo', det_gen.tel_what1 AS 'telefono_1', det_gen.tel_what2 AS 'telefono_2' FROM det_gen`,(err,rows)=>{
             if (err) reject(err)
@@ -12,7 +13,7 @@ const getGenerals = () =>{
 }
 
 // model fot get banners generals 
-const getBannersGenerals = () =>{
+const getBannersGenerals = async() =>{
     return new Promise((resolve, reject)=>{
         db.query(`SELECT bann_g.bann_imagen as 'url_imagen'  FROM bann_g WHERE (SELECT CURRENT_TIMESTAMP) BETWEEN f_inicial AND f_final`,(err,rows)=>{
             if (err) reject(err)
